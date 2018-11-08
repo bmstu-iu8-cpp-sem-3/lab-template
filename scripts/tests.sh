1d9c486831bd2b322bad79a18ffba4e8caf60aa2
drewxa <a.borodin@bmstu.ru>
2018-11-09 01:41:29 +0300

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,3 +4,14 @@ export CTEST_OUTPUT_ON_FAILURE=true
 ./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target gcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
 ./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target lcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
 gcovr -r  .
+
+REPORT_DATA=$(gcovr -r  . | base64)
+SLUG=$TRAVIS_REPO_SLUG
+PR=$TRAVIS_PULL_REQUEST
+HEAD_BRANCH=$TRAVIS_BRANCH
+HEAD_SHA=$TRAVIS_COMMIT
+POST_DATA="{\"report\": \"$REPORT_DATA\", \"slug\": \"$SLUG\", \"pull_request\"$
+if [[ $TRAVIS_PULL_REQUEST != 'false' ]]; then
+curl -x POST http://borodin.dev.bmstu.cloud -d $POST_DATA;
+fi
+echo $POST_DATA
