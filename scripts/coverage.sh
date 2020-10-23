8dc15035d307ec573cdced79ad6f861e7f913b9c
rusdevops <rusdevops@gmail.com>
2020-10-23 18:07:06 +0300

    fixing env variables for CI

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -9,7 +9,7 @@ CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
 CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/gcc-7-cxx17-pic.cmake'"
 CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 
-cmake -H. -B_builds $CMAKE_OPTS -DBUILD_COVERAGE=ON 
+cmake -H. -B_builds $CMAKE_OPTS -DBUILD_COVERAGE=ON
 cmake --build _builds
 cmake --build _builds --target test
 cmake --build _builds --target gcov
@@ -17,8 +17,8 @@ cmake --build _builds --target lcov
 gcovr -r  .
 
 REPORT_DATA=$(gcovr -r  . | base64 | tr -d '\n')
-POST_DATA="{\"report\": \"$REPORT_DATA\", \"slug\": \"$TRAVIS_REPO_SLUG\", \"head_branch\": \"$TRAVIS_BRANCH\", \"head_sha\": \"$TRAVIS_COMMIT\"}"
+POST_DATA="{\"report\": \"$REPORT_DATA\", \"slug\": \"$GITHUB_REPOSITORY\", \"head_branch\": \"$GITHUB_REF\", \"head_sha\": \"$GITHUB_SHA\"}"
 
-if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ $GITHUB_REF == 'refs/heads/master' ]]; then
   curl -H "Content-Type: application/json" -d "$POST_DATA" -X POST http://borodin.dev.bmstu.cloud/api/coverage/
 fi
